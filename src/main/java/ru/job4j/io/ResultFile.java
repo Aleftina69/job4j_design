cb94540492fbfdd679f260a40632a11145d6a340
Brux <okladno94@gmail.com>
2024-10-30 00:28:29 +0500

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,8 +7,8 @@ public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
             int[][] table = new int[10][10];
-            for (int i = 0; i <= 10; i++) {
-                for (int j = 0; j <= 10; j++) {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
                     String line = i + " * " + j + " = " + (i * j) + "    ";
                     output.write(line.getBytes());
                 }
