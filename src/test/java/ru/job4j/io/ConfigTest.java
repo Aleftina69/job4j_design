fb11c8a5f0f116ff4cae96716eac98901e3673d4
Brux <okladno94@gmail.com>
2024-11-04 13:18:58 +0500

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenFileWithCommentsAndEmptyLines() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+        assertThat(config.value("role")).isEqualTo("Developer");
+    }
+
+    @Test
+    void whenInvalidPattern() {
+        String path = "./data/invalid_pattern.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenKeyWithMultipleEquals() {
+        String path = "./data/pair_with_multiple_equals.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key")).isEqualTo("value=1");
+    }
+}
