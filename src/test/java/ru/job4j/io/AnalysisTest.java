6706cdbdb4a1b962d1a3f80d1b71bebb6cafa555
Brux <okladno94@gmail.com>
2024-11-05 13:57:23 +0500

    8. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class AnalysisTest {
+
+    @Test
+    void unavailable(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("200 10:56:01");
+            output.println("500 10:57:01");
+            output.println("400 10:58:01");
+            output.println("300 10:59:01");
+            output.println("500 11:01:02");
+            output.println("200 11:02:02");
+        }
+        File target  = tempDir.resolve("target.txt").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader input = new BufferedReader(new FileReader(target))) {
+            input.lines().forEach(result::append);
+        }
+        String expected = "10:57:01;10:59:01" + System.lineSeparator() +
+                          "11:01:02;11:02:02" + System.lineSeparator();
+        assertThat(result.toString().equals(expected));
+    }
+
+    @Test
+    void unavailableIncorrect(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source.txt").toFile();
+        try (PrintWriter output = new PrintWriter(source)) {
+            output.println("200 10:56:01");
+            output.println("500 10:57:01");
+            output.println("400 10:58:01");
+            output.println("300 10:59:01");
+            output.println("500 11:01:02");
+            output.println("200 11:02:02");
+        }
+        File target  = tempDir.resolve("target.txt").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader input = new BufferedReader(new FileReader(target))) {
+            input.lines().forEach(result::append);
+        }
+        String expected = "10:57:01;11:59:01" + System.lineSeparator() +
+                          "11:01:02;11:00:02" + System.lineSeparator();
+        assertThat(result.toString()).isNotEqualTo(expected);
+    }
+}
