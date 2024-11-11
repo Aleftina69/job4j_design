f51a1a875250b053f862fccf60f55cc96120a283
Brux <okladno94@gmail.com>
2024-11-11 22:07:43 +0500

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,12 +1,16 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -32,10 +36,12 @@ public class EchoServer {
                             output.flush();
                         }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        } catch (IOException e) {
+            LOG.error("Exception while handling client connection", e);
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception while starting server", e);
         }
     }
 }
