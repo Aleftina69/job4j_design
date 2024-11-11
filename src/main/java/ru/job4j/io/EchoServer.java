0fb70bb3c02ad33f024c0df600625342d3eaf2b0
Brux <okladno94@gmail.com>
2024-11-11 14:26:35 +0500

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -11,17 +12,25 @@ public class EchoServer {
                 Socket socket = server.accept();
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
+                             new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
                     String string = input.readLine();
-                    System.out.println(string);
-                    if (string != null && string.contains("msg=Bye")) {
-                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        System.out.println("Server closed.");
-                        output.flush();
-                        break;
-                    } else {
-                        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        output.flush();
+                    if (string != null) {
+                        if (string.contains("msg=Exit")) {
+                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            output.write("Завершить работу сервера.".getBytes());
+                            output.flush();
+                            break;
+                        }
+                        if (string.contains("msg=Hello")) {
+                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            output.write("Hello, dear friend.".getBytes());
+                            output.flush();
+                        }
+                        if (string.contains("msg=Any")) {
+                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            output.write("What, dear friend?".getBytes());
+                            output.flush();
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
