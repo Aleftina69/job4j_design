9ccc6d649decebcc7b0822f169feb916b93dccd5
Brux <okladno94@gmail.com>
2024-11-11 22:33:18 +0500

    2. Что такое Socket? [#4850 #521155]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,27 +18,26 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    output.flush();
                     String string = input.readLine();
                     if (string != null) {
                         if (string.contains("msg=Exit")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                             output.write("Завершить работу сервера.".getBytes());
                             output.flush();
                             break;
                         }
                         if (string.contains("msg=Hello")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                             output.write("Hello, dear friend.".getBytes());
                             output.flush();
                         }
                         if (string.contains("msg=Any")) {
-                            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                             output.write("What, dear friend?".getBytes());
                             output.flush();
                         }
                     }
-        } catch (IOException e) {
-            LOG.error("Exception while handling client connection", e);
+                } catch (IOException e) {
+                    LOG.error("Exception while handling client connection", e);
                 }
             }
         } catch (IOException e) {
