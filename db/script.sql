8e2657a3cd93c76e9f5dfb6e0b09923f7721a240
Brux <okladno94@gmail.com>
2024-12-03 12:54:36 +0500

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,4 @@
+insert into book(author, title, price, availability) values ('Булгаков М. А.', 'Мастер и Маргарита', 2000, true);
+update book set price = 2200;
+delete from book;
+select * from book;
